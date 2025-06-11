96ecbffa9ec3050f6cfb169ce0a03854a2668797
JavierDeBenavides <javierbbdeq@gmail.com>
2025-06-11 18:00:52 +0200

    Added multiplying feature

--- pythoncode.py
+++ pythoncode.py
@@ -18,3 +18,18 @@ result = num1 + num2
 
 # Result printing
 print("The sum is:", result)
+
+# Making a simple calculator to multiply numbers
+# Input reading
+num1 = input("Introduce your first number: ")
+num2 = input("Introduce your second number: ")
+
+# String to float conversion
+num1 = float(num1)
+num2 = float(num2)
+
+# Multiplying
+result2 = num1 * num2
+
+# Result printing
+print("The sum is:", result)
